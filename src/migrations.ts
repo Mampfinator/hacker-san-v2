e8d77eaed5b6641e79a46d8d2406ab8981d64de6
Mampfinator <46071499+Mampfinator@users.noreply.github.com>
2022-12-20 03:41:51 +0100

    Fixed migrations

--- src/migrations.ts
+++ src/migrations.ts
@@ -1,5 +1,5 @@
-import { InitialMigration1661942360000 } from "migrations/1661942360000-InitialMigration";
-import { ChannelMigration1661942362285 } from "migrations/1661942362285-ChannelMigration";
+import { InitialMigration1661942360000 } from "../migrations/1661942360000-InitialMigration";
+import { ChannelMigration1661942362285 } from "../migrations/1661942362285-ChannelMigration";
 
 export const Migrations = [
     InitialMigration1661942360000,
--- typeorm.config.ts
+++ typeorm.config.ts
@@ -2,8 +2,7 @@ import {DataSource} from "typeorm";
 import {ConfigService} from "@nestjs/config";
 import config from "./src/modules/config/config";
 import { Entities } from "./src/entities";
-import { ChannelMigration1661942362285 } from "./migrations/1661942362285-ChannelMigration";
-import { Migrations } from "src/migrations";
+import { Migrations } from "./src/migrations";
 
 const configService = new ConfigService(config());
 
