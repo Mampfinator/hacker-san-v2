8e4cd1e8ace149ae052584f8c335528b8b69a138
Mampfinator <46071499+Mampfinator@users.noreply.github.com>
2023-01-07 09:43:17 +0100

    Don't let me work at 3am if want separated commits

--- src/modules/platforms/events/video.event.ts
+++
@@ -1,8 +0,0 @@
-import { IEvent } from "@nestjs/cqrs";
-import { ChannelEntity } from "../models/channel.entity";
-import { StreamEntity } from "../models/stream.entity";
-
-export class VideoEvent implements IEvent {
-    public channel: ChannelEntity;
-    public video: StreamEntity;
-}
