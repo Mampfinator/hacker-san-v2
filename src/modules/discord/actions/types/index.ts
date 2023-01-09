488894a9d78346d3efe10a82f049d18334440a06
Mampfinator <46071499+Mampfinator@users.noreply.github.com>
2023-01-09 05:44:13 +0100

    Removed unfinished action (TBI in the future)

--- src/modules/discord/actions/types/index.ts
+++ src/modules/discord/actions/types/index.ts
@@ -2,6 +2,5 @@ import { EchoAction } from "./echo.action";
 import { LockAction } from "./lock.action";
 import { NotifyAction } from "./notify.action";
 import { RenameAction } from "./rename.action";
-import { ThreadAction } from "./thread.action";
 
-export const ActionTypes = [EchoAction, LockAction, NotifyAction, RenameAction, ThreadAction];
+export const ActionTypes = [EchoAction, LockAction, NotifyAction, RenameAction];
