b4dcaf8cd9a7df0b37043ffd74c20397816bb1f2
Mampfinator <46071499+Mampfinator@users.noreply.github.com>
2023-01-22 14:52:40 +0100

    feat: reimplemented /quick-setup rename

--- src/modules/discord/slash-command/types/quick-setup/quick-setup.command.ts
+++ src/modules/discord/slash-command/types/quick-setup/quick-setup.command.ts
@@ -1,17 +1,23 @@
+import { CommandBus } from "@nestjs/cqrs";
 import { InjectRepository } from "@nestjs/typeorm";
-import { Channel as DiscordChannel, ChannelType, ChatInputCommandInteraction, Role as DiscordRole } from "discord.js";
+import { Channel as DiscordChannel, ChatInputCommandInteraction, Colors, EmbedBuilder, GuildChannel, Role as DiscordRole } from "discord.js";
 import { Repository } from "typeorm";
 import { Platform } from "../../../../../constants";
+import { EnsureChannelCommand } from "../../../../platforms/commands/ensure-channel.command";
 import { ActionDescriptor } from "../../../models/action.entity";
 import { PLATFORM_CHOICES } from "../../../util";
 import { Command } from "../../decorators/command.decorator";
 import { Interaction } from "../../decorators/interaction.decorator";
 import { Boolean, Channel, Role, String } from "../../decorators/option.decorator";
 import { SlashCommand } from "../../decorators/slash-command.decorator";
+import { CHANNELID_OPTIONS, PLATFORM_OPTIONS } from "../../slash-command.constants";
 
 @SlashCommand({ name: "quick-setup", description: "Quickly set up actions for a specific channel." })
 export class QuickSetupCommand {
-    constructor(@InjectRepository(ActionDescriptor) actions: Repository<ActionDescriptor>) {}
+    constructor(
+        private readonly commandBus: CommandBus,
+        @InjectRepository(ActionDescriptor) private readonly actions: Repository<ActionDescriptor>,
+    ) {}
 
     @Command({ name: "general", description: "General setup for a channel." })
     general(
@@ -40,18 +46,72 @@ export class QuickSetupCommand {
             description: "Use temporary chats in stream-chat for all stream related actions.",
         })
         useTempThreads?: boolean,
-    ) {}
+    ) {
+
+    }
 
     @Command({ name: "rename", description: "Set up live indicator emojis for a channel." })
-    rename(
-        @Channel({ name: "channel", description: "The channel to set up", required: true, channel_types: [] })
-        channel: DiscordChannel,
+    async rename(
+        @String(PLATFORM_OPTIONS) platform: Platform,
+        @String(CHANNELID_OPTIONS) channelId: string,
+        @Channel({ name: "for-channel", description: "The channel to set up", required: true })
+        discordChannel: GuildChannel,
+        @Interaction() interaction: ChatInputCommandInteraction,
         @String({
             name: "base-name",
-            description: "Base name for the channel. If not set, defaults to provided channel's name.",
+            description: "Will be prefixed with 🔴/⚫. Defaults to selected channel's name.",
         })
         baseName?: string,
-    ) {}
+    ) {
+        await interaction.deferReply();
+        
+        const name = baseName ?? discordChannel.name;
+        const {success, channel} = await this.commandBus.execute(new EnsureChannelCommand(channelId, platform));
+        const { guildId } = interaction;
+
+        if (!success) return {embeds: [ new EmbedBuilder().setColor(Colors.Red).setTitle("Setup failed").setDescription("This likely means that the channel you were attempting to set up does not exist.")]}
+
+        const actions = [
+            this.actions.create({
+                guildId,
+                discordChannelId: discordChannel.id,
+                channelId: channel.platformId,
+                platform,
+                type: "rename",
+                onEvent: "live",
+                data: {
+                    name: `🔴 ${name}`
+                }
+            }), 
+            this.actions.create({
+                guildId,
+                discordChannelId: discordChannel.id,
+                channelId: channel.platformId,
+                platform,
+                type: "rename",
+                onEvent: "offline",
+                data: {
+                    name: `⚫ ${name}`
+                }
+            }),
+        ]
+
+        try {
+            await this.actions.insert(actions);
+        } catch {
+            return {embeds: [ new EmbedBuilder().setColor(Colors.Red).setTitle("Setup failed").setDescription("Failed creating new actions. This is likely an internal error, or you already have those same actions set up. If the problem persists, reach out to the bot developer!")]}
+        }
+
+        return {
+            embeds: [
+                new EmbedBuilder()
+                    .setColor(Colors.Green)
+                    .setTitle("Actions created")
+                    .setDescription(`Created two rename actions for ${channel.name} (${channel.platform}, ${channel.platformId}):`)
+                    .addFields(...actions.map(action => action.toEmbedField()))
+            ]
+        }
+    }
 
     @Command({ name: "threads-migration", description: "Migrate setup for a stream to threads!" })
     migrateToThreads(
